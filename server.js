d78cfb9b66d17aa8d9bdca3ab62c1e52a587787b
ricardopg1987 <45467321+ricardopg1987@users.noreply.github.com>
2023-05-16 08:43:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run' + new Date())
+    res.end('Hello Cloud Run')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
